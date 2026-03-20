14d16b5b6742f97d695ba715e6f14a7744570690
ADS 36134652025.1M <36134652025.1M@senacrjedu.local>
2026-03-20 11:53:31 -0300

    2026-03-20

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { EnderecoService } from '../services/endereco.service';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {
 
+export class Cadastro {
+  private enderecoService = inject(EnderecoService);
+  
+  passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+
+  form = new FormGroup({
+    nome: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    senha: new FormControl('',[Validators.required, Validators.pattern(this.passwordRegex)]),
+    cep: new FormControl(''),
+    logradouro: new FormControl('')
+});
+
+onSubmit(){
+  if(this.form.valid)
+    console.log("Formulário Válido: ", this.form.value)
+  else
+    console.log("Formulário Inválido: ", this.form.value)
 }
+
+regraMinimoCaractere:boolean = false;
+regraMaximoCaractere:boolean = false;
+regraLetraMaiuscula:boolean = false;
+regraCaractereEspecial:boolean = false;
+regraLetraNumero:boolean = false;
+
+
+validarSenha(senha: string): void{
+
+  //Regex das regras
+  const rxMin8 = /^.{8,}$/;
+  const rxMax16 = /^.{0,16}$/;
+  const rxMaiuscula = /[A-Z]/;
+  const rxEspecial = /[^a-zA-z0-9]/;
+  const rxNumero = /\d/;
+  const rxLetra = /[A-Za-z]/;
+
+  this.regraMinimoCaractere = rxMin8.test(senha);
+  this.regraMaximoCaractere = rxMax16.test(senha);
+  this.regraLetraMaiuscula = rxMaiuscula.test(senha);
+  this.regraCaractereEspecial = rxEspecial.test(senha);
+  this.regraLetraNumero = rxNumero.test(senha) && rxLetra.test(senha);
+}
+
+get buscarCep(){
+  const cep = this.form.get('cep')?.value ?? ''
+  this.enderecoService.getEndereco(cep).subscribe(endereco => {
+    this.form.get('logradouro')?.setValue(endereco.logradouro);
+  });
+return true;
+}
+}
+
---
+++ src/app/services/endereco.service.ts
@@ -0,0 +1,19 @@
+import {Injectable, inject} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Endereco } from "../models/endereco.model";
+import { Observable, catchError, throwError} from "rxjs";
+
+@Injectable ({providedIn:'root'})
+export class EnderecoService{
+    private http = inject (HttpClient);
+
+    getEndereco(cep: string): Observable<Endereco>{
+        return this.http.get<Endereco>("http://viacep.com.br/ws/"+cep+"/json").pipe(
+            catchError(err => {
+                console.log("Erro ao buscar CEP:", err);
+                return throwError(() => new Error("Falha ao buscar CEP."));
+
+            })
+        )
+    }
+}
